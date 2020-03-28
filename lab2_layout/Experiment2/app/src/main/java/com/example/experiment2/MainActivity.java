4f107c6d3eee761dbb53444035a6af833017ab82
“Caicaixian <951513648@qq.com>
2020-03-28 13:48:17 +0800

    “Experiment

--- lab2_layout/Experiment2/app/src/main/java/com/example/experiment2/MainActivity.java
+++ lab2_layout/Experiment2/app/src/main/java/com/example/experiment2/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.experiment2;
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
